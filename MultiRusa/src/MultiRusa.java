8946ccde0de2c4a9389f204e0dc9e6b38df0e211
josmellbu <josmell.quispe@upeu.edu.pe>
2020-04-25 22:04:45 -0500

    version 2

--- MultiRusa/src/MultiRusa.java
+++ MultiRusa/src/MultiRusa.java
@@ -1,19 +1,30 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class MultiRusa{
     public static List<Integer> array=new ArrayList<>();
         
     public void Multi(int num1, int num2){
-        if (num1 >= 1){
+        if (num1 >= 1) {
             if (num1%2 !=0) {
-                array.add(num2);
-            }
+                    array.add(num2);
+                }
             Multi(num1/2, num2*2);
         }
     }
 
-    public static void main(String[] args){ 
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Ingrese el num1: ");
+        int num1=sc.nextInt();
+        System.out.print("Ingrese el num2: ");
+        int num2=sc.nextInt();
+                
         MultiRusa ru=new MultiRusa();
+        ru.Multi(num1, num2);
+        
+        System.out.println("El resultado es: ");
+        System.exit(0);
     }
 }
